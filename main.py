bc3b7277fcda176d51d8d772947dfaac4e4de3ea
Ahmed Bahaa <ahmedbahaa.3@hotmail.com>
2021-07-17 19:21:44 +0200

    added error handlers

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,request,jsonify
 app = Flask(__name__)
 
 
@@ -8,3 +8,30 @@ def short_links():
         return get_short_links()
     else:
         return post_short_link(request.json)
+
+
+@app.route('/shortlinks/<slug>', methods=['PUT'])
+def update_slug(slug):
+    return update_short_link(slug, request.json)
+
+
+@app.errorhandler(400)
+def bad_request(e):
+    response = {
+        "status": "FAILED",
+        "message": "BAD REQUEST"
+    }
+    return jsonify(response), 400
+
+@app.errorhandler(404)
+@app.errorhandler(405)
+def page_not_found(e):
+    response = {
+        "status": "FAILED",
+        "message": "NOT FOUND"
+    }
+    return jsonify(response), 404
+
+@app.errorhandler(500)
+def bad_request(e):
+    return jsonify({}), 500
