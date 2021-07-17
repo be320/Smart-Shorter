2e8e10591e276f18ed0083936d700a1a1b36797c
Ahmed Bahaa <ahmedbahaa.3@hotmail.com>
2021-07-17 19:13:17 +0200

    Added Shortlinks Route

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from flask import Flask
-
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello():
-    return "<h1>hello world</h1>"
+@app.route('/shortlinks', methods=['GET', 'POST'])
+def short_links():
+    if request.method == 'POST':
+        return get_short_links()
+    else:
+        return post_short_link(request.json)
